4eabc8fb58e82e8aa236296271d55cfb646177d6
Pedro Hidalgo <pedro.n.h.lagopuelo@gmail.com>
2025-11-11 16:10:46 -0300

    Agrego clases Libro y FichaBibliografica

---
+++ prog2_TPI/src/edu/utn/entities/FichaBibliografica.java
@@ -0,0 +1,13 @@
+package edu.utn.entities;
+
+public class FichaBibliografica {
+    
+    private Long id;
+    private boolean eliminado;
+    private String isbn;
+    private String clasificacionDewey;
+    private String estanteria;
+    private String idioma;
+    
+    
+}
---
+++ prog2_TPI/src/edu/utn/entities/Libro.java
@@ -0,0 +1,14 @@
+package edu.utn.entities;
+
+public class Libro {
+    
+    private Long id;
+    private boolean eliminado;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private Integer anioEdicion;
+    private FichaBibliografica fichaBibliografica;
+    
+    
+}
