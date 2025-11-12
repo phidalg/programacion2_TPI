a5162666eef2bb91e44e8ee8a79a6d71ea307dd7
Pedro Hidalgo <pedro.n.h.lagopuelo@gmail.com>
2025-11-12 02:55:43 -0300

    Termino de completar entidades

--- prog2_TPI/src/edu/utn/entities/FichaBibliografica.java
+++ prog2_TPI/src/edu/utn/entities/FichaBibliografica.java
@@ -8,6 +8,72 @@ public class FichaBibliografica {
     private String clasificacionDewey;
     private String estanteria;
     private String idioma;
-    
+
+    public FichaBibliografica(String isbn, String clasificacionDewey, String estanteria, String idioma) {
+        this.isbn = isbn;
+        this.clasificacionDewey = clasificacionDewey;
+        this.estanteria = estanteria;
+        this.idioma = idioma;
+    }
+
+    public FichaBibliografica(Long id, boolean eliminado, String isbn, String clasificacionDewey, String estanteria, String idioma) {
+        this.id = id;
+        this.eliminado = eliminado;
+        this.isbn = isbn;
+        this.clasificacionDewey = clasificacionDewey;
+        this.estanteria = estanteria;
+        this.idioma = idioma;
+    }
+
+    public FichaBibliografica() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getClasificacionDewey() {
+        return clasificacionDewey;
+    }
+
+    public String getEstanteria() {
+        return estanteria;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setClasificacionDewey(String clasificacionDewey) {
+        this.clasificacionDewey = clasificacionDewey;
+    }
+
+    public void setEstanteria(String estanteria) {
+        this.estanteria = estanteria;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
     
 }
--- prog2_TPI/src/edu/utn/entities/Libro.java
+++ prog2_TPI/src/edu/utn/entities/Libro.java
@@ -9,6 +9,82 @@ public class Libro {
     private String editorial;
     private Integer anioEdicion;
     private FichaBibliografica fichaBibliografica;
-    
+
+    public Libro(String titulo, String autor, String editorial, Integer anioEdicion, FichaBibliografica ficha) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.anioEdicion = anioEdicion;
+        this.fichaBibliografica = ficha;
+    }
+
+    public Libro(Long id, boolean eliminado, String titulo, String autor, String editorial, Integer anioEdicion, FichaBibliografica ficha) {
+        this.id = id;
+        this.eliminado = eliminado;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.anioEdicion = anioEdicion;
+        this.fichaBibliografica = ficha;
+    }
+
+    public Libro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public Integer getAnioEdicion() {
+        return anioEdicion;
+    }
+
+    public FichaBibliografica getFichaBibliografica() {
+        return fichaBibliografica;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setAnioEdicion(Integer anioEdicion) {
+        this.anioEdicion = anioEdicion;
+    }
+
+    public void setFichaBibliografica(FichaBibliografica fichaBibliografica) {
+        this.fichaBibliografica = fichaBibliografica;
+    }
     
 }
